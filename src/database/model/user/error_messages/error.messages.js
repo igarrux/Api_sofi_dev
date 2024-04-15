bda4a991a7a898c3e8456a732d940b2a658ad27e
Jhon Guerrero <garruxx@gmail.com>
2024-04-14 21:40:57 -0500

    fix: :bug: Se creo el archivo error messages faltante

---
+++ src/database/model/user/error_messages/error.messages.js
@@ -0,0 +1,7 @@
+export const errors = {
+	passwordTooShort: 'La contraseña debe de tener minimo 8 carácteres',
+	invalidEmail: 'El correo no es valido',
+	nameTooShort: 'El nombre es demasiado corto',
+	emailEmpty: 'El correo no es valido',
+	passwordEmpty: 'La contraseña no puede estar vacía',
+}
