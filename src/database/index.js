aa6894a71f90bad305728e1fe23e7313ca900c6c
Jhon Guerrero <garruxx@gmail.com>
2024-04-15 08:22:46 -0500

    refactor: :loud_sound: Se reinician los colores de los logs cuando se imprimen

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import { connect } from 'mongoose'
 export const connectDB = async () => {
 	try {
 		await connect(process.env.MONGO_DB_URL)
-		console.log('\x1b[36mDatabase connected')
+		console.log('\x1b[36mDatabase connected \x1b[0m')
 	} catch (error) {
 		throw new Error('Database connection failed', error)
 	}
--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 3000
 config()
 
 app.listen(port, () => {
-    console.log(`\x1b[35mApp listening on port ${port}`)
+    console.log(`\x1b[35mApp listening on port ${port}\x1b[0m`)
 })
