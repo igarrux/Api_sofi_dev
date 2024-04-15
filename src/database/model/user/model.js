7aba7eca66d0632a2cd16344176ffa6463f8d91a
Jhon Guerrero <garruxx@gmail.com>
2024-04-14 22:02:19 -0500

    feat: :sparkles: Se aplica una función hash a la contraseña antes de guardarla

--- src/database/model/user/model.js
+++ src/database/model/user/model.js
@@ -2,10 +2,12 @@ import { model, Schema } from 'mongoose'
 import { defineName } from './definitions/name.js'
 import { defineEmail } from './definitions/email.js'
 import { definePassword } from './definitions/password.js'
+import { hastPassword } from './parsers/password.js'
 
 const userSchema = new Schema({
 	name: defineName,
 	email: defineEmail,
 	password: definePassword,
 })
+userSchema.pre('save', hastPassword)
 export const User = model('users', userSchema)
