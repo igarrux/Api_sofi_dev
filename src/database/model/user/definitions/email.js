4b832fa7fb65bface143b6965cd84d229130fa55
Jhon Guerrero <garruxx@gmail.com>
2024-04-14 21:38:39 -0500

    feat: :safety_vest: se creo la validación del email

---
+++ src/database/model/user/definitions/email.js
@@ -0,0 +1,12 @@
+import { errors } from '../error_messages/error.messages.js'
+const validate = {
+	validator: (text) => /^(\D(\w|\.)+@(\D)\w+\.\D(\w|\.)+)$/.test(text),
+	message: errors.invalidEmail,
+}
+
+export const defineEmail = {
+	unique: true,
+	type: String,
+	required: [true, errors.emailEmpty],
+	validate,
+}
