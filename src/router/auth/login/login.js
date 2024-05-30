2dc0a22e5e35b4ff828dae1c30b96197a456138b
Jhon (Garrux) <igarrux@gmail.com>
2024-05-29 23:40:33 -0500

    feat(auth): :sparkles: Se modifica Login a una función. Ya no se usa con router

--- src/router/auth/login/login.js
+++ src/router/auth/login/login.js
@@ -1,15 +1,13 @@
-import express from 'express'
 import { getJWT, validateCredentials } from './utils/index.js'
 import { cookieConfig } from './config.js'
-const router = express.Router()
 
-router.post('/login', async (req, res) => {
+export const Login = async (req, res) => {
+	console.log('helo');
+	if (!req.body) return res.status(400).send()
 	const userId = await validateCredentials(req.body)
 	if (!userId) return res.status(401).cookie('session', '').send()
 
 	const jwt = getJWT(userId)
 	res.cookie('session', jwt, cookieConfig)
 	res.status(200).send()
-})
-
-export default router
+}
