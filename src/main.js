a44cd48ffcb8d379638189f3baa0d5b9253a2376
Jhon (Garrux) <igarrux@gmail.com>
2024-05-29 23:41:58 -0500

    feat(main): :sparkles: Se agrega el enrutador

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { config } from 'dotenv'
 import { connectDB } from './database/index.js'
+import router from './router/router.js'
 const app = express()
 const port = process.env.PORT || 3000
 config()
 connectDB()
 
+app.use('/', router)
 app.listen(port, () => {
-    console.log(`\x1b[35mApp listening on port ${port}\x1b[0m`)
+	console.log(`\x1b[35mApp listening on port ${port}\x1b[0m`)
 })
