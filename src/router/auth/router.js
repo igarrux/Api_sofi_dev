35fb51ea289bead0682712cecea01690dee10c26
Jhon (Garrux) <igarrux@gmail.com>
2024-05-29 23:48:39 -0500

    feat(auth): :sparkles: Se agrega singup al router de auth

--- src/router/auth/router.js
+++ src/router/auth/router.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { Login } from './login/login.js'
+import { SingUp } from './signup/singup.js'
 const router = Router()
 
 router.post('/login', Login)
-
+router.post('/singup', SingUp)
 export { router as authRouter }
