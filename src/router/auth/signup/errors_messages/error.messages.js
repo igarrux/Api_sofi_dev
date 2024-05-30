8a0a232a2a37c9fad2fb24f8382a75ce2f65a1a5
Jhon (Garrux) <igarrux@gmail.com>
2024-05-30 00:27:55 -0500

    feat(auth/signup): :sparkles: Se crean los mensajes de error para el singup

---
+++ src/router/auth/signup/errors_messages/error.messages.js
@@ -0,0 +1,5 @@
+export const errors = {
+    emailDiplicated: "Ese email ya está registrado.",
+    bodyEmpty: "El cuerpo de la respuesta no puede estar vacío",
+    interlSeverError: "Error interno del servidor"
+}
