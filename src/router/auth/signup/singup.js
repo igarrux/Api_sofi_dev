65ec9d455aa976a8feb686cc0c68549214300c6e
Jhon (Garrux) <igarrux@gmail.com>
2024-05-30 00:28:11 -0500

    feat(auth): :sparkles: Se completa el singup

--- src/router/auth/signup/singup.js
+++ src/router/auth/signup/singup.js
@@ -1,4 +1,19 @@
+import { User } from '../../../database/index.js'
+import { errors } from './errors_messages/error.messages.js'
 
-export const SingUp = (req, res)=>{
-    
+export const SingUp = async (req, res) => {
+	if (!req.body) return res.status(400).send(errors.bodyEmpty)
+	const user = new User(req.body)
+
+	try {
+		await user.save()
+	} catch (error) {
+		if (error.code === 11000) res.status(409).send(errors.emailDiplicated)
+		if (error.name === 'ValidationError') res.status(400).send(error.errors)
+		if (error && error.name != 'ValidationError' && error.code != 11000) {
+			res.status(500).send(errors.interlSeverError)
+		}
+		return
+	}
+	res.status(201).send()
 }
