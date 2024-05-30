197f175c8a523e2c67083f4b93e1bf7acddaee4a
Jhon (Garrux) <igarrux@gmail.com>
2024-05-29 21:59:37 -0500

    feat:(main) :sparkles: Conexíon a la base de datos Se agrega la conexión a la base de datos en el main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { config } from 'dotenv'
+import { connectDB } from './database/index.js'
 const app = express()
 const port = process.env.PORT || 3000
 config()
+connectDB()
 
 app.listen(port, () => {
     console.log(`\x1b[35mApp listening on port ${port}\x1b[0m`)
