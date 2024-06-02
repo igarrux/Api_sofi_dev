88d9f33ee70f5cbeff683b2991e1ebd21c202497
Jhon Guerrero <garruxx@gmail.com>
2024-06-01 23:34:18 -0500

    feat(main): :sparkles: Se configura cookie parser

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import express from 'express'
 import { config } from 'dotenv'
 import { connectDB } from './database/index.js'
 import router from './router/router.js'
+import cookieParser from 'cookie-parser'
 const app = express()
 const port = process.env.PORT || 3000
 config()
 connectDB()
+
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use('/', router)
 app.listen(port, () => {
 	console.log(`\x1b[35mApp listening on port ${port}\x1b[0m`)
