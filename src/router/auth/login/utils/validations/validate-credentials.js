85e97981d21bc3df71239b8691a10d1aca395f24
Jhon Guerrero <garruxx@gmail.com>
2024-06-01 22:51:13 -0500

    fix(validations): :bug: ahora se valida que password y email, para evitar que la app se rompa

--- src/router/auth/login/utils/validations/validate-credentials.js
+++ src/router/auth/login/utils/validations/validate-credentials.js
@@ -1,7 +1,8 @@
 import { compare } from 'bcrypt'
 import { User } from '../../../../../database/index.js'
 export const validateCredentials = async (user) => {
+	if (!user.password || !user.email) return false
 	const { password, _id } = await User.findOne({ email: user.email })
 	const passwordIsValid = await compare(user.password, password)
-    return passwordIsValid? _id: false
+	return passwordIsValid? _id: false
 }
