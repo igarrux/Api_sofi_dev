526cda236cc0e7058898d891e5cb97f158bff392
Jhon Guerrero <garruxx@gmail.com>
2024-06-01 23:35:25 -0500

    feat(auth): :sparkles: Se indica a post: card/add que use el guard

--- src/router/cards/router.js
+++ src/router/cards/router.js
@@ -2,9 +2,10 @@ import { Router } from 'express'
 import { AddCard } from './add/add.js'
 import { upload } from './utils/multer/upload.js'
 import { multerErrorHandler } from './utils/multer/error_handler.js'
+import { guard } from '../../guard.js'
 const router = Router()
 
-router.post('/add', upload.single('thumbnail'), AddCard)
+router.post('/add', guard, upload.single('thumbnail'), AddCard)
 
 router.use(multerErrorHandler)
 export { router as cardsRouter }
