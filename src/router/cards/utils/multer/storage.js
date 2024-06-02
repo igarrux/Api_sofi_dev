5d2c50f5b96fed2380d82a05f12f235abd1e35f9
Jhon Guerrero <garruxx@gmail.com>
2024-06-02 00:46:05 -0500

    feat(multer): :sparkles: ahora el storage de multer elimina las imagenes previas al actualizar

--- src/router/cards/utils/multer/storage.js
+++ src/router/cards/utils/multer/storage.js
@@ -3,6 +3,7 @@ import { diskStorage, MulterError } from 'multer'
 import { extname } from 'path'
 import { ERRORS } from './messages/error.js'
 import { mkdirIfNotExist } from './mkdir_If_not_exist.js'
+import { deleteSync } from 'del'
 
 const destination = async (req, _, cb) => {
 	if (!req.user_id) {
@@ -17,12 +18,16 @@ const destination = async (req, _, cb) => {
 }
 
 const filename = (req, file, cb) => {
-	req.body._id = req.body._id ?? new Types.ObjectId()
-	const EXT = extname(file.originalname)
-	const FILE_NAME = req.body._id.toString() + EXT
-
-	req.body.thumbnail = FILE_NAME
-	cb(null, FILE_NAME)
+	try {
+		req.body._id = req.body._id ?? new Types.ObjectId()
+		const EXT = extname(file.originalname)
+		const FILE_NAME = req.body._id.toString() + EXT
+		deleteSync(`public/${req.user_id}/images/${req.body._id}.*`)
+		req.body.thumbnail = FILE_NAME
+		cb(null, FILE_NAME)
+	} catch (error) {
+		cb(new MulterError(ERRORS.UPLOAD_GENERAL_ERROR))
+	}
 }
 
 export const multerStorage = diskStorage({ destination, filename })
