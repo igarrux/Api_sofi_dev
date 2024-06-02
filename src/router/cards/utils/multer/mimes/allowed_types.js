d0c910e1d5038fcc1171cdf93367b7bbf896eb68
Jhon Guerrero <garruxx@gmail.com>
2024-06-01 22:52:08 -0500

    feat(multer): :sparkles: Array con los tipos de imagenes admitidos

---
+++ src/router/cards/utils/multer/mimes/allowed_types.js
@@ -0,0 +1,10 @@
+export const ALLOWED_TYPES = [
+    'image/jpeg',
+    'image/png',
+    'image/svg+xml',
+    'image/jpg',
+    'image/apng',
+    'image/gif',
+    'image/webp',
+    'image/avif'
+]
