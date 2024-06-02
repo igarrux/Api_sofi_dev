135ce83b645058141ffa43216f18bfcf4f6adac3
Jhon Guerrero <garruxx@gmail.com>
2024-06-01 22:55:51 -0500

    feat(http): :sparkles: Se agrega el enrutador de cards al enrutador principal

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { authRouter } from './auth/router.js'
+import { cardsRouter } from './cards/router.js'
+
 const router = express.Router()
 
 router.use('/auth/', authRouter)
+router.use('/cards/', cardsRouter)
 
 export default router
