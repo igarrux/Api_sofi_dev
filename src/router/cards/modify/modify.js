372e06b85cbe5483ddd9d5e580617b59ec2f785f
Jhon Guerrero <garruxx@gmail.com>
2024-06-03 21:24:02 -0500

    fix(http/card): :ambulance: Cualquier usuario administrador podía modificar las tarjetas de otro. Arreglado

--- src/router/cards/modify/modify.js
+++ src/router/cards/modify/modify.js
@@ -1,9 +1,20 @@
+import { isObjectIdOrHexString } from 'mongoose'
 import { Card } from '../../../database/model/cards/index.js'
 import { errorsMapper } from '../../utils/mappers/errors.mapper.js'
+import { upload } from '../utils/multer/upload.js'
 
-export const ModifyCard = async (req, res) => {
+export const ModifyCard = async (req, res, next) => {
+	const { id } = req.params
+	if (!isObjectIdOrHexString(id)) return res.status(400).send()
 	try {
-		await Card.findByIdAndUpdate(req.body._id, req.body)
+		const card = await Card.findById(req.params.id)
+		if (!card) return res.status(404).send()
+		if (card.owner != req.user_id) return res.status(401).send()
+		await card.save()
+		upload.single('thumbnail')(req, res, (err) => {
+			if (err) return next(err)
+			res.status(204).send()
+		})
 	} catch (error) {
 		if (error.name === 'ValidationError') {
 			res.status(400).send(errorsMapper(error.errors))
@@ -12,5 +23,4 @@ export const ModifyCard = async (req, res) => {
 		res.status(500).send()
 		return
 	}
-	res.status(201).send()
 }
