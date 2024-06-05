1d2636f2a09a7f5d13981f9ea648b9924955cb7e
Jhon (Garrux) <igarrux@gmail.com>
2024-06-05 00:26:21 -0500

    feat(http/card): :sparkles: Se crea la funcion para eliminar tarjetas

---
+++ src/router/cards/remove/remove.js
@@ -0,0 +1,14 @@
+import { deleteSync } from 'del'
+import { Card } from '../../../database/model/cards/index.js'
+export const RemoveCard = async (req, res) => {
+	try {
+		const { id } = req.params
+		deleteSync(`public/${req.user_id}/images/${id}.*`)
+		const card = await Card.findById(id)
+		if (card.owner != req.user_id) return res.status(401).send()
+		await card.deleteOne()
+		res.status(200).send()
+	} catch {
+		res.status(500).send()
+	}
+}
--- src/router/cards/router.js
+++ src/router/cards/router.js
@@ -4,10 +4,12 @@ import { upload } from './utils/multer/upload.js'
 import { multerErrorHandler } from './utils/multer/error_handler.js'
 import { guard } from '../../guard.js'
 import { ModifyCard } from './modify/modify.js'
+import { RemoveCard } from './remove/remove.js'
+import multer from 'multer'
 const router = Router()
 
-router.post('/add', guard, upload.single('thumbnail'), AddCard)
-router.put('/modify', guard, upload.single('thumbnail'), ModifyCard)
-
+router.post('/', guard, upload.single('thumbnail'), AddCard)
+router.patch('/:id', guard, ModifyCard)
+router.delete('/:id', guard, RemoveCard)
 router.use(multerErrorHandler)
 export { router as cardsRouter }
