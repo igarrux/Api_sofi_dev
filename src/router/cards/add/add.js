b104993e7db9ef5ff0e407e3d37cb2300b8a2391
Jhon (Garrux) <igarrux@gmail.com>
2024-06-07 16:20:37 -0500

    fix(http/card): :bug: Se agrega el await faltante en el addCard

--- src/router/cards/add/add.js
+++ src/router/cards/add/add.js
@@ -1,11 +1,12 @@
 import { Card } from '../../../database/model/cards/index.js'
 import { errorsMapper } from '../../utils/mappers/errors.mapper.js'
 
-export const AddCard = (req, res) => {
-	const cardData = { ...req.body, owner: req.user_id }
-	const card = new Card(cardData)
+export const AddCard = async (req, res) => {
 	try {
-		card.save()
+		const cardData = { ...req.body, owner: req.user_id }
+		const card = new Card(cardData)
+		await card.save()
+		res.status(201).send()
 	} catch (error) {
 		if (error.name === 'ValidationError') {
 			res.status(400).send(errorsMapper(error.errors))
@@ -14,5 +15,4 @@ export const AddCard = (req, res) => {
 		res.status(500).send()
 		return
 	}
-	res.status(201).send()
 }
