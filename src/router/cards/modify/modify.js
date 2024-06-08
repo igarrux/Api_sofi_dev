9bea13d99eccf43f9e3d969c7bbdbc0b50215363
Jhon (Garrux) <igarrux@gmail.com>
2024-06-07 21:46:54 -0500

    fix(http/card): :bug: Las tarjetas solo actualizaban la imagen. corregido

--- src/router/cards/modify/modify.js
+++ src/router/cards/modify/modify.js
@@ -5,14 +5,20 @@ import { dbHttpError } from '../../utils/mappers/db_http_errors.mapper.js'
 
 export const ModifyCard = async (req, res, next) => {
 	const { id } = req.params
-	if (!isObjectIdOrHexString(id)) return res.status(400).send()
 	try {
+		if (!isObjectIdOrHexString(id)) return res.status(400).send()
+		//Verify if the card exists
 		const card = await Card.findById(req.params.id)
 		if (!card) return res.status(404).send()
+		//Verify if the card belongs to the user
 		if (card.owner != req.user_id) return res.status(401).send()
-		await card.save()
-		upload.single('thumbnail')(req, res, (err) => {
+
+		//Update the thumbnail
+		upload.single('thumbnail')(req, res, async (err) => {
 			if (err) return next(err)
+			const { _id, ...newData } = req.body
+			//Update the card
+			await card.updateOne(newData)
 			res.status(204).send()
 		})
 	} catch (error) {
