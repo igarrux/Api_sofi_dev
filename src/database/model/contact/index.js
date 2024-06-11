655a0555844d2ce40a703639b83d3f2cc5a465c7
Jhon (Garrux) <igarrux@gmail.com>
2024-06-10 23:55:13 -0500

    chore(database): :fire: Se elimina el modelo de contacto

--- src/database/model/contact/index.js
+++
@@ -1,9 +0,0 @@
-import { model, Schema, ObjectId } from 'mongoose'
-
-const contactSchema = new Schema({
-	content: String,
-	title: String,
-    sender: String,
-	owner: ObjectId,
-})
-export const Contact = model('contact-forms', contactSchema)
