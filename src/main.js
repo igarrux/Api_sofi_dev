e50adbf688c19c4f8e23063e0928c3242fb46b1f
Jhon Guerrero <garruxx@gmail.com>
2024-06-12 21:12:03 -0500

    fix(http/public): :bug: Faltaba exponer los archivos de /public. resuelto

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use('/', router)
+app.use(express.static('public'))
 app.listen(port, () => {
 	console.log(`\x1b[35mApp listening on port ${port}\x1b[0m`)
 })
