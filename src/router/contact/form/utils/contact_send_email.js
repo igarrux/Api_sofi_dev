12b957c98eb75ad1dd850a1900cb099004c00421
Jhon (Garrux) <igarrux@gmail.com>
2024-06-25 19:02:55 -0500

    fix: :bug: No se informaba del error al usuario. Solucionado

--- src/router/contact/form/utils/contact_send_email.js
+++ src/router/contact/form/utils/contact_send_email.js
@@ -3,6 +3,7 @@ import { User } from '../../../../database/index.js'
 import { sendMail } from '../../../../email/email.js'
 import { logger } from '../../../../logger.js'
 import { ERRORS } from '../messages/errors.js'
+import { httpError } from '../../../utils/mappers/http_errors.js'
 config()
 const EMAIL_DOMAIN = process.env.EMAIL_DOMAIN
 if (!EMAIL_DOMAIN) throw new Error('EMAIL_DOMAIN not found, check .env')
@@ -39,5 +40,6 @@ export const contactSendEmail = async (req, res) => {
 		})
 	} catch (error) {
 		logger.Error(error, ERRORS.MAILER_ERROR)
+		httpError(500, ERRORS.MAILER_ERROR, 'http/contact')(res)
 	}
 }
