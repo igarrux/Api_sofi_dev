8ad2b89b359634c8c720ab1536e023b139ebd1b5
Jhon (Garrux) <igarrux@gmail.com>
2024-07-05 19:40:01 -0500

    fix(database): :fire: Se remueve el dropDups

--- src/database/model/user/type_definitions/user_name.js
+++ src/database/model/user/type_definitions/user_name.js
@@ -2,9 +2,6 @@ import { ERRORS } from './messages/errors.js'
 export const UserName = {
 	type: String,
 	required: [true, ERRORS.USER_NAME_EMPTY],
-	index: {
-		unique: true,
-		dropDups: true,
-	},
+	unique: true,
 	minlength: [3, ERRORS.USER_NAME_TOO_SHORT],
 }
