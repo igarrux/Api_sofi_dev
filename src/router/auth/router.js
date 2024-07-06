8040431dc26ce041ce3105afd9406c3ed3972f8b
Jhon (Garrux) <igarrux@gmail.com>
2024-07-05 23:31:15 -0500

    feat(http/auth): :sparkles: Se agrega el path a user info

--- src/router/auth/router.js
+++ src/router/auth/router.js
@@ -5,11 +5,13 @@ import { Logout } from './logout/logout.js'
 import { GetUserInfo } from './user/get_user_info.js'
 import { guard } from '../../guard.js'
 import { verificationCode } from './verification_code/verification_code.js'
+import { updateUser } from './update_user/update_user.js'
 const router = Router()
 
 router.post('/login', Login)
 router.post('/singup', SingUp)
 router.get('/logout', Logout)
 router.get('/user-info', guard, GetUserInfo)
+router.patch('/user-info/:user_name', updateUser)
 router.get('/verification/:user_name', verificationCode)
 export { router as authRouter }
