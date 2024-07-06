26c47d08fd9091aab0bd80521ab1e51445bb9a90
Jhon (Garrux) <igarrux@gmail.com>
2024-07-05 22:15:05 -0500

    feat(http/verification): :sparkles: se crean los errores

---
+++ src/router/auth/verification_code/messages/errors.js
@@ -0,0 +1,5 @@
+export const ERRORS = {
+	USER_NAME_EMPTY: 'Se requiere un nombre de usuario',
+	MAILER_ERROR: 'hubo un problema al enviar el correo',
+	INTERNAL_ERROR: 'algo paso al enviar la verificación. Intenta de nuevo',
+}
