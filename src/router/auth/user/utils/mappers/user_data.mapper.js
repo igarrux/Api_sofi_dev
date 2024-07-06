f19c2a9bc3b87e8d26a675bb9cf983af8a475155
Jhon Guerrero <garruxx@gmail.com>
2024-07-06 18:49:22 -0500

    feat(http/get-user-info): :sparkles: se actualiza para devolver la foto del usuario

--- src/router/auth/user/utils/mappers/user_data.mapper.js
+++ src/router/auth/user/utils/mappers/user_data.mapper.js
@@ -2,5 +2,6 @@ export const userDataMapper = (userDBData) => ({
 	id: userDBData._id,
 	email: userDBData.email,
 	name: userDBData.name,
-	user_name: userDBData.user_name
+	user_name: userDBData.user_name,
+	profile_img: `/profiles/${userDBData.user_name}.${userDBData.img_ext}`
 })
