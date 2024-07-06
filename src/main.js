2654e13309c07d6b10b1f0720ab9c8c5f9616de9
Jhon (Garrux) <igarrux@gmail.com>
2024-07-05 20:34:43 -0500

    refactor(main): :recycle: Solo se idento

--- src/main.js
+++ src/main.js
@@ -10,13 +10,19 @@ const port = process.env.PORT || 3000
 config()
 connectDB()
 
-if (!process.env.COOKIE_SECRET || !process.env.JWT_SECRET || !process.env.COOKIE_SECRET)
+if (
+	!process.env.COOKIE_SECRET ||
+	!process.env.JWT_SECRET ||
+	!process.env.COOKIE_SECRET
+)
 	throw new Error('Missing enviroment variables')
 
-app.use(cors({
-	credentials: true,
-	origin: process.env.CORS_ORIGIN.split(',')
-}))
+app.use(
+	cors({
+		credentials: true,
+		origin: process.env.CORS_ORIGIN.split(','),
+	})
+)
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use('/', router)
