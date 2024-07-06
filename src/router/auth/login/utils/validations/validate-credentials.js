104984d4e5ce29acf2c0180f977fa721747ee7ec
Jhon Guerrero <garruxx@gmail.com>
2024-07-06 00:28:47 -0500

    feat(http/login): :sparkles: Se agrega una validacion extra en caso de que el usuario no exista

--- src/router/auth/login/utils/validations/validate-credentials.js
+++ src/router/auth/login/utils/validations/validate-credentials.js
@@ -3,6 +3,7 @@ import { User } from '../../../../../database/index.js'
 export const validateCredentials = async (user) => {
 	if (!user.password || !user.email) return false
 	const { password, _id } = await User.findOne({ email: user.email })
+	if (!password || !_id) return false
 	const passwordIsValid = await compare(user.password, password)
-	return passwordIsValid? _id: false
+	return passwordIsValid ? _id : false
 }
