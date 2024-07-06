a9ca6f2090c88f5d689d1cfb13edf766e24e90a6
Jhon (Garrux) <igarrux@gmail.com>
2024-07-05 22:19:12 -0500

    feat(http/auth): :sparkles: Se agrega la ruta para obtener la verificacion

--- src/router/auth/router.js
+++ src/router/auth/router.js
@@ -4,10 +4,12 @@ import { SingUp } from './signup/singup.js'
 import { Logout } from './logout/logout.js'
 import { GetUserInfo } from './user/get_user_info.js'
 import { guard } from '../../guard.js'
+import { verificationCode } from './verification_code/verification_code.js'
 const router = Router()
 
 router.post('/login', Login)
 router.post('/singup', SingUp)
 router.get('/logout', Logout)
 router.get('/user-info', guard, GetUserInfo)
+router.get('/verification/:user_name', verificationCode)
 export { router as authRouter }
