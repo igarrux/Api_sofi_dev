1bdbfd5ef7c6c8f42bde3b589591ae5ba022f613
Jhon Guerrero <garruxx@gmail.com>
2024-07-15 19:27:18 -0500

    feat(cookies): :sparkles: El sameSite de las cookies cambia a flex

--- src/router/auth/login/cookie-config.js
+++ src/router/auth/login/cookie-config.js
@@ -2,6 +2,7 @@ const isCookieSecure = process.env.IS_COOKIE_SECURE == 'true'
 export const cookieConfig = {
 	httpOnly: true,
 	secure: isCookieSecure,
+	sameSite: 'flex',
 	signed: true,
 	maxAge: 1000 * 60 * 60 * 24 * 90,
 }
