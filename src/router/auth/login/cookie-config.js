27f8300f4dd59ca0e67d27791780410a53be14c6
Jhon (Garrux) <igarrux@gmail.com>
2024-07-19 22:37:39 -0500

    feat(cookie): :bug: SameSite cambia a None

--- src/router/auth/login/cookie-config.js
+++ src/router/auth/login/cookie-config.js
@@ -2,7 +2,7 @@ const isCookieSecure = process.env.IS_COOKIE_SECURE == 'true'
 export const cookieConfig = {
 	httpOnly: true,
 	secure: isCookieSecure,
-	sameSite: 'flex',
+	sameSite: 'None',
 	signed: true,
 	maxAge: 1000 * 60 * 60 * 24 * 60,
 }
