0e61db44d8ca538afe1a32fba6e42a7555e9a15d
Jhon Guerrero <garruxx@gmail.com>
2024-07-30 18:28:30 -0500

    feat(databse/user): :sparkles: Se agrega timestaps al userSchema
    \

--- src/database/model/user/index.js
+++ src/database/model/user/index.js
@@ -5,17 +5,20 @@ import { Password } from './type_definitions/password.js'
 import { hastPassword } from './parsers/password.js'
 import { UserName } from './type_definitions/user_name.js'
 
-const userSchema = new Schema({
-	name: Name,
-	email: Email,
-	password: Password,
-	id: String,
-	user_name: UserName,
-	img_ext: {
-		type: String,
-		default: "none"
-	}
-})
+const userSchema = new Schema(
+	{
+		name: Name,
+		email: Email,
+		password: Password,
+		id: String,
+		user_name: UserName,
+		img_ext: {
+			type: String,
+			default: 'none',
+		},
+	},
+	{ timestamps: true }
+)
 userSchema.pre('save', hastPassword)
 userSchema.pre('updateOne', hastPassword)
 export const User = model('users', userSchema)
