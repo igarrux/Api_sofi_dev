9abf3318eebb79ac79b412a4bd722e5ba7caea72
Jhon Guerrero <garruxx@gmail.com>
2024-07-30 16:42:56 -0500

    fix(saveUpdateErrorHandler): :bug: No se manejaban los errores de subida de archivo correctamente

--- src/router/auth/utils/save_update_error_handler/save_update_error_handler.js
+++ src/router/auth/utils/save_update_error_handler/save_update_error_handler.js
@@ -2,6 +2,7 @@ import { ERRORS } from './messages/error.messages.js'
 import { httpError } from '../../../utils/mappers/http_errors.js'
 import { dbHttpError } from '../../../utils/mappers/db_http_errors.mapper.js'
 import { logger } from '../../../../logger.js'
+import { MulterError } from 'multer'
 export const saveUpdateErrorHandler = (error, res) => {
 	const { code, name, keyPattern, errors } = error
 	if (code === 11000 && keyPattern.email) {
@@ -11,9 +12,11 @@ export const saveUpdateErrorHandler = (error, res) => {
 		return httpError(409, ERRORS.USER_NAME_DUPLICATED, 'user_name')(res)
 	}
 	if (name === 'ValidationError') return dbHttpError(errors, 400)(res)
-
+	if (error instanceof MulterError) {
+		return httpError(error.code, error.field, 'image')(res)
+	}
 	if (name != 'ValidationError' && code != 11000) {
 		res.status(500).send(ERRORS.INTERNAL_ERROR)
 	}
-	logger.Error("save-update",error)
+	logger.Error('save-update', error)
 }
